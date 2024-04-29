499f8fb7b40047fc6ec8a2bc566f41f96a30a087
sc20srn <116085538+sc20srn@users.noreply.github.com>
2024-04-29 07:37:15 +0100

    Updated News Articles in Header.

--- cpp codebase/UI/HeaderWidget/HeaderWidget.cpp
+++ cpp codebase/UI/HeaderWidget/HeaderWidget.cpp
@@ -4,8 +4,7 @@
 #include <QDir>
 #include <QFontDatabase>
 
-HeaderWidget::HeaderWidget(DBHandler *handler, OrderBook *orderBook, QWidget *parent)
-    : QWidget(parent), handler(handler), orderBook(orderBook) {
+HeaderWidget::HeaderWidget(DBHandler *handler, OrderBook *orderBook, QWidget *parent) : QWidget(parent), handler(handler), orderBook(orderBook) {
     initialiseLayoutAndAddButtons();
     this->setStyleSheet("background-color: rgba(12, 83, 45, 90); border-radius: 20px;");
 }
@@ -48,22 +47,22 @@ void HeaderWidget::setupLogoAndCompany() {
     }
     QString family = QFontDatabase::applicationFontFamilies(id).at(0);
     QFont titleFont(family, 18);
-    //QFont titleFont("Helvetica", 24, QFont::Bold);
+    // QFont titleFont("Helvetica", 24, QFont::Bold);
     titleLabel = new QLabel("University of Leeds Stock Exchange");
     titleLabel->setFont(titleFont);
     titleLabel->setAlignment(Qt::AlignCenter);
     titleLabel->setStyleSheet("QLabel { color : white; }");
 
     QPixmap imagePixmap;
-    
+
     // If running on Xcode, the directory is usually the Debug folder
     if (QDir::currentPath().contains("Debug")) {
         imagePixmap = QPixmap("../../resources/UoLSE_Logo.png");
     } else {
         imagePixmap = QPixmap("COMP5530M/cpp codebase/resources/UoLSE_Logo.png");
     }
-    
-//    QPixmap imagePixmap("COMP5530M/cpp codebase/resources/UoLSE_Logo.png");
+
+    //    QPixmap imagePixmap("COMP5530M/cpp codebase/resources/UoLSE_Logo.png");
     imageLabel = new QLabel;
     imageLabel->setPixmap(imagePixmap.scaled(150, 90, Qt::KeepAspectRatio));
 }
@@ -104,12 +103,12 @@ void HeaderWidget::setupModelType() {
     modelType->setStyleSheet("QComboBox { color: white; background-color: #444444; border-radius: 5px; padding: 3px; } QComboBox::drop-down { border: none; }");
 }
 
-
 void HeaderWidget::setupNewsTicker() {
-    int rightMargin = -800;  // Margin from the right edge
+    
+    int rightMargin = -800; // Margin from the right edge
     int widgetWidth = 320;  // Width of the newsTicker
     int widgetHeight = 30;  // Height of the newsTicker
-    int yPos = 45;  // Vertical position from the top
+    int yPos = 45;          // Vertical position from the top
 
     newsTicker = new QLabel(this);
     // Set background color, text color, and round corners directly on the newsTicker
@@ -120,8 +119,15 @@ void HeaderWidget::setupNewsTicker() {
 
     // Set geometry with the calculated x-position
     newsTicker->setGeometry(QRect(xPos, yPos, widgetWidth, widgetHeight));
-    
-    newsItems << "Bitcoin hits new all-time high!" << "Major exchange suffers breach, Bitcoin safe." << "Bitcoin suffers major bear run" << "Global regulators propose new rules for Bitcoin transactions." << "Tech giants plan to integrate Bitcoin for payments." << "Environmental concerns rise over Bitcoin mining energy use.";
+
+    newsItems << "Ranaex Exchange Unveils RNAC: A Pioneering Leap in Cryptocurrency"
+              << "Lessons From The FTX Trial: Regulating CEXs May Not Be Enough To Prevent Bad Actors | CryptoInfoNet"
+              << "Potential Shiba Inu Rally as Selling Slows, Uniswap & AI Altcoin Strengthen Position"
+              << "Global regulators propose new rules for Bitcoin transactions."
+              << "FIU Notifies Fresh Money Laundering, Terror Financing Reporting Alerts For Capital Markets"
+              << "Greenhaven Road Capital Main Fund Q1 2024 Investor Letter"
+              << "Digging into Decentralised Exchanges: Benefits and Challenges"
+              << "Institutional Crypto Investors Choose ETFSwap (ETFS) As The Better Alternative To Binance And ByBit";
     newsTicker->setText(newsItems.first());
 
     newsTimer = new QTimer(this);
@@ -129,12 +135,10 @@ void HeaderWidget::setupNewsTicker() {
     newsTimer->start(3000);
 }
 
-
 void HeaderWidget::updateNewsTicker() {
     currentNewsIndex = (currentNewsIndex + 1) % newsItems.size();
     newsTicker->setText(newsItems.at(currentNewsIndex));
     newsTicker->setAlignment(Qt::AlignCenter);
-
 }
 
 void HeaderWidget::connectButtons() {
@@ -145,13 +149,15 @@ void HeaderWidget::connectButtons() {
 void HeaderWidget::startSim() {
     std::string option = modelType->currentText().toStdString();
 
-    if (option == "GAN Model"){
-      cancelSim();
-      Gan::startServer(handler, orderBook);
+    if (option == "GAN Model") {
+        cancelSim();
+        Gan::startServer(handler, orderBook);
     }
 }
 
-void HeaderWidget::cancelSim(){
+void HeaderWidget::cancelSim() {
     Gan::cancelRequested = true;
-    if (Gan::startGenerate.joinable()) { Gan::startGenerate.join(); }
+    if (Gan::startGenerate.joinable()) {
+        Gan::startGenerate.join();
+    }
 }
